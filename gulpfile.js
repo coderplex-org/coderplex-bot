5cdca22ff4acd10b576c466d46dd3bae694a05a2
buoyantair <buoyantair@protonmail.com>
2019-02-21 17:25:25 +0530

    Update build setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,26 @@
-const gulp = require('gulp');
+const gulp = require("gulp");
 const ts = require("gulp-typescript");
 const tslint = require("gulp-tslint");
-const { resolve } = require("path");
 
 const tsProject = ts.createProject("tsconfig.json");
 
-gulp.task("ts", () => (
-  tsProject.src()
+gulp.task("ts", () =>
+  tsProject
+    .src()
     .pipe(tsProject())
     .js.pipe(gulp.dest("dist"))
-));
+);
 
-gulp.task('tslint', () => (
-  tsProject.src()
-    .pipe(tslint({
-      formatter: 'verbose'
-    }))
+gulp.task("tslint", () =>
+  tsProject
+    .src()
+    .pipe(
+      tslint({
+        formatter: "verbose"
+      })
+    )
     .pipe(tslint.report())
-));
-gulp.task('watch', () => (
-  gulp.watch('src/**/*.ts', ['ts'])
-));
+);
+gulp.task("watch", () => gulp.watch("src/**/*.ts", ["ts"]));
 
-gulp.task("default", [ "ts"]);
+gulp.task("default", ["ts"]);
